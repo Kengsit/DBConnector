22413a741f0b3d0a9b1d5cf25a826b2341b1d6d0
dulyasit <dulyasit@hotmail.com>
2018-10-27 21:02:37 +0700

    First Commit

---
+++ DBConnectorUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DBConnectorUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<string>[] result;
+            DBConnector.DBConnector db = new DBConnector.DBConnector();
+            result = db.Select();
+            Assert.IsTrue(result.Length > 0);
+        }
+    }
+}
